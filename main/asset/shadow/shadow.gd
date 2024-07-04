f5b4a12b8adcb893fa62a159a98a301fc408b90a
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-03 20:09:48 -0500

    Player character

--- main/asset/shadow/shadow.gd
+++ main/asset/shadow/shadow.gd
@@ -1,4 +1,4 @@
 class_name Shadow extends Sprite2D
 
 func _ready() -> void:
-	modulate = Color(0, 0, 0, 0.5)
+	modulate = Color(0, 0, 0, 0.7)
--- main/player/player.gd
+++ main/player/player.gd
@@ -1,6 +1,9 @@
 class_name Player extends CharacterBody2D
 
 @export var sprite: Sprite2D
+@export var walk_anim: AnimationPlayer
+@export var step_particles: CPUParticles2D
+@export var step_player: AudioStreamPlayer
 
 @export var speed: float = 64.0
 
@@ -13,6 +16,11 @@ func _physics_process(delta: float) -> void:
 	
 	animate(input)
 
+func step() -> void:
+	step_particles.direction = -velocity.normalized()
+	step_particles.restart()
+	step_particles.emitting = true
+	step_player.play()
 
 func animate(input: Vector2) -> void:
 	if input.x < 0 and input.y < 0:
@@ -32,3 +40,8 @@ func animate(input: Vector2) -> void:
 		sprite.frame = 5
 	elif input.x < 0:
 		sprite.frame = 7
+	
+	if is_zero_approx(input.length()):
+		walk_anim.play("idle")
+	else:
+		walk_anim.play("walk") 
